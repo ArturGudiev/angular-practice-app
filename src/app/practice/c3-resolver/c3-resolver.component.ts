b303dab98011508d99095aa5f9de126ab84d37d5
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-06 12:09:12 +0300

    Commit

---
+++ src/app/practice/c3-resolver/c3-resolver.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {BookInterface, BookTrackerError} from "../c2-throw-custom-http-error/interfaces";
+import {C3Service} from "./c3.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {C3BookInterface, C3BookTrackerError} from "./interfaces_and_constants";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-c3-resolver',
+  templateUrl: './c3-resolver.component.html',
+  styleUrls: ['./c3-resolver.component.sass']
+})
+export class C3ResolverComponent implements OnInit {
+  errorMessage = ''
+  books: C3BookInterface[] = [];
+
+  constructor(public c3Service: C3Service, private http: HttpClient, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.c3Service.getBooksError().subscribe({
+      next: (books: C3BookInterface[] | C3BookTrackerError) => this.books = <C3BookInterface[]>books,
+      error: (err: C3BookTrackerError) => this.errorMessage = err.friendlyMessage
+      }
+    )
+  }
+
+}
