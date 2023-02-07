95eece45a0cbe7c356c13bba59c093172dd44152
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-07 10:26:46 +0300

    Commit

--- src/app/practice/c2-throw-custom-http-error/c2-throw-custom-http-error.component.ts
+++ src/app/practice/c2-throw-custom-http-error/c2-throw-custom-http-error.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {C2ServiceService} from "./c2-service.service";
-import {BookInterface, BookTrackerError} from "./interfaces";
+import {C2BookInterface, C2BookTrackerError} from "./interfaces";
 
 @Component({
   selector: 'app-c2-throw-custom-http-error',
@@ -8,15 +8,15 @@ import {BookInterface, BookTrackerError} from "./interfaces";
   styleUrls: ['./c2-throw-custom-http-error.component.sass']
 })
 export class C2ThrowCustomHttpErrorComponent implements OnInit {
-  private books!: BookInterface[];
+  private books!: C2BookInterface[];
 
   constructor(public c2Service: C2ServiceService) { }
 
   ngOnInit(): void {
     this.c2Service.getBooks().subscribe(
       {
-        next: books => this.books = <BookInterface[]>books,
-        error: (err: BookTrackerError) => console.log('AAA', err)
+        next: books => this.books = <C2BookInterface[]>books,
+        error: (err: C2BookTrackerError) => console.log('AAA', err)
       }
     );
   }
