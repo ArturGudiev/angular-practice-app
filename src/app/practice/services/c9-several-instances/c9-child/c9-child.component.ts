8487fb611433e1155a6cae174acdc0844c7e6e4b
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-09 00:34:34 +0300

    prepare components

--- src/app/practice/services/c9-several-instances/c9-child/c9-child.component.ts
+++ src/app/practice/services/c9-several-instances/c9-child/c9-child.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {C9ServiceService} from "./c9-service.service";
 
 @Component({
   selector: 'app-c9-child',
   templateUrl: './c9-child.component.html',
-  styleUrls: ['./c9-child.component.sass']
+  styleUrls: ['./c9-child.component.sass'],
+  providers: [
+    C9ServiceService
+  ]
 })
 export class C9ChildComponent implements OnInit {
+  @Input() val: number = 0;
 
-  constructor() { }
+  constructor(public service: C9ServiceService) { }
 
   ngOnInit(): void {
+    this.service.a = this.val;
   }
 
 }
