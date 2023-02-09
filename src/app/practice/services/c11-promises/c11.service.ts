0eea2b50b4bc0ef856684c322759b19cd4f8af43
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-09 09:31:55 +0300

    prepare components

--- src/app/practice/services/c11-promises/c11.service.ts
+++ src/app/practice/services/c11-promises/c11.service.ts
@@ -8,13 +8,15 @@ export class C11Service {
   constructor() { }
 
   async getPromise(num: number): Promise<any> {
-    await setTimeout(()=> {} , 1000);
-    new Promise(async (resolved, rejected) => {
-      if (num > 0){
-        resolved('Everything is good')
-      } else {
-        rejected("It's bad");
-      }
+    // await setTimeout(()=> {} , 1000);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (num > 0) {
+          resolve('Everything is good')
+        } else {
+          reject("It's bad");
+        }
+      }, 1000);
     })
   }
 }
