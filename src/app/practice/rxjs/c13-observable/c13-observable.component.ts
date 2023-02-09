e2b796eaf96706ddb70ec8bafaf2d4bc0155d87c
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-09 14:18:47 +0300

    COmmit service

--- src/app/practice/rxjs/c13-observable/c13-observable.component.ts
+++ src/app/practice/rxjs/c13-observable/c13-observable.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs";
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {delay, from, fromEvent, interval, Observable, of, take} from "rxjs";
 
 
 @Component({
@@ -7,13 +7,30 @@ import {Observable} from "rxjs";
   templateUrl: './c13-observable.component.html',
   styleUrls: ['./c13-observable.component.sass']
 })
-export class C13ObservableComponent implements OnInit {
-
+export class C13ObservableComponent implements OnInit, AfterViewInit {
+  @ViewChild('para') par!: ElementRef;
+  dataWithDelay$: Observable<number[]> = of([]);
   constructor() { }
 
   ngOnInit(): void {
-    // myObservable$.subscribe(val => console.log(val));
-    // myObservable$.subscribe(val => console.log('2', val));
+    this.dataWithDelay$ = this.getDataWithDelay();
+    // 1
+
+    // 2  --- 1, 2 and [1,2]
+
+
+  }
+
+  ngAfterViewInit(): void {
+    // 3 from event
+
+    // 4 first 2 and check completion
+  }
+
+  getDataWithDelay(): Observable<number[]> {
+    return of([1, 2, 3]).pipe(
+      delay(2000)
+    );
   }
 
 }
