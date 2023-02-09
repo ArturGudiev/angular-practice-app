a04845d1e9e08fee6b12b1258cb9d2c80e7b718a
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-09 08:50:18 +0300

    prepare components

--- src/app/practice/services/c10-catch-error/c10-catch-error.module.ts
+++ src/app/practice/services/c10-catch-error/c10-catch-error.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { C10CatchErrorRoutingModule } from './c10-catch-error-routing.module';
 import { C10CatchErrorComponent } from './c10-catch-error.component';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { C10CatchErrorComponent } from './c10-catch-error.component';
   ],
   imports: [
     CommonModule,
-    C10CatchErrorRoutingModule
+    C10CatchErrorRoutingModule,
+    HttpClientModule
   ]
 })
 export class C10CatchErrorModule { }
