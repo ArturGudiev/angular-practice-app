510a48d0f434d3f478526c722af5e1bb11aa81cd
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-10 18:27:31 +0300

    Prepare fist subtask of 15 task

--- src/app/practice/rxjs/c15-rxjs-operators/c15-rxjs-operators.component.ts
+++ src/app/practice/rxjs/c15-rxjs-operators/c15-rxjs-operators.component.ts
@@ -9,14 +9,32 @@ import {Observable, of} from "rxjs";
 })
 export class C15RxjsOperatorsComponent implements OnInit {
   array$: Observable<any[]> = of([]);
+
+  // o2_every_second$: Observable<number> = this.c15Service.o2_every_second;
+  // o2_every_3_seconds: Observable<number> = this.c15Service.o2_every_3_seconds;
+
+  o3_every_second: Observable<number> = this.c15Service.o3_every_second;
+  o3_every_2_seconds: Observable<number> = this.c15Service.o3_every_2_seconds;
+
   constructor(public c15Service: C15Service) {
   }
 
   ngOnInit(): void {
     // 1 map array
-    this.array$ = this.c15Service.getObservableArray();
+    // this.array$ = this.c15Service.getObservableArray();
+
+    // 2 get latest from any o2** observables once anyone emits
+    // this.o3_every_second.subscribe(val => console.log('o3_every_second', val));
+    // this.o3_every_2_seconds.subscribe(val => console.log('o3_every_2_seconds', val));
 
 
+    // 3
+
+    // 4
+
+    // 5
+    // interval(1000)
+    //   .subscribe(val => console.log('interval', val))
   }
 
 }
--- src/app/practice/rxjs/c15-rxjs-operators/c15.service.ts
+++ src/app/practice/rxjs/c15-rxjs-operators/c15.service.ts
@@ -6,7 +6,73 @@ import {Observable, of} from "rxjs";
 })
 export class C15Service {
 
-  constructor() { }
+  o2_every_second: Observable<number> = new Observable((subscriber) => {
+    let i = 0;
+    setInterval(() => {
+      i++;
+      console.debug('o2_every_second', i);
+      subscriber.next(i)
+    }, 1000);
+    setTimeout(() => subscriber.complete(), 15000)
+  })
+
+  o2_every_3_seconds: Observable<number> = new Observable((subscriber) => {
+    let i = 0;
+    setInterval(() => {
+      i++;
+      console.debug('o2_every_3_seconds', i);
+      subscriber.next(i)
+    }, 3000);
+  })
+
+  o3_every_second: Observable<number> = new Observable((subscriber) => {
+    let i = 0;
+    setInterval(() => {
+      i++;
+      // console.debug('o3_every_second', i);
+      subscriber.next(i)
+    }, 1000);
+    setTimeout(() => subscriber.complete(), 5000)
+  })
+  o3_every_2_seconds: Observable<number> = new Observable((subscriber) => {
+    let i = 1;
+    setInterval(() => {
+      i *= 2;
+      // console.debug('o3_every_2_seconds', i);
+      subscriber.next(i)
+    }, 2000);
+    setTimeout(() => subscriber.complete(), 5000)
+
+  })
+
+  o40_main_observable: Observable<number> = new Observable((subscriber) => {
+    let i = 0;
+    setInterval(() => {
+      i += 10;
+      // console.debug('o3_every_second', i);
+      subscriber.next(i)
+    }, 3000);
+  })
+  o41_every_2_seconds: Observable<number> = new Observable((subscriber) => {
+    let i = 0;
+    setInterval(() => {
+      i += 20;
+      // console.debug('o3_every_2_seconds', i);
+      subscriber.next(i)
+    }, 2000);
+  })
+
+  o42_every_second: Observable<number> = new Observable((subscriber) => {
+    let i = 0;
+    setInterval(() => {
+      i += 30;
+      // console.debug('o3_every_2_seconds', i);
+      subscriber.next(i)
+    }, 1000);
+  });
+
+
+    constructor() { }
 
 
   getObservableArray(): Observable<any[]> {
