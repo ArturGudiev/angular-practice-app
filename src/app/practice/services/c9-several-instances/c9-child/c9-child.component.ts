b2a3f86a787525ebde68619fd77dcfc127af4bea
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-11 13:55:21 +0300

    Create c18

--- src/app/practice/services/c9-several-instances/c9-child/c9-child.component.ts
+++ src/app/practice/services/c9-several-instances/c9-child/c9-child.component.ts
@@ -5,9 +5,6 @@ import {C9ServiceService} from "./c9-service.service";
   selector: 'app-c9-child',
   templateUrl: './c9-child.component.html',
   styleUrls: ['./c9-child.component.sass'],
-  providers: [
-    C9ServiceService
-  ]
 })
 export class C9ChildComponent implements OnInit {
   @Input() val: number = 0;
--- src/app/practice/services/c9-several-instances/c9-several-instances.module.ts
+++ src/app/practice/services/c9-several-instances/c9-several-instances.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { C9SeveralInstancesRoutingModule } from './c9-several-instances-routing.module';
 import { C9SeveralInstancesComponent } from './c9-several-instances.component';
 import { C9ChildComponent } from './c9-child/c9-child.component';
+import {C9ServiceService} from "./c9-child/c9-service.service";
 
 
 @NgModule({
@@ -14,6 +15,9 @@ import { C9ChildComponent } from './c9-child/c9-child.component';
   imports: [
     CommonModule,
     C9SeveralInstancesRoutingModule
+  ],
+  providers: [
+    C9ServiceService
   ]
 })
 export class C9SeveralInstancesModule { }
