24137c34ec392f74132f26165119ccbeb8e2cc53
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-11 13:35:43 +0300

    Create c18

---
+++ src/app/practice/services/c8-providers/c8-data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {C8LoggerService} from "./c8-logger.service";
+
+@Injectable()
+export class C8DataService {
+
+  constructor(public loggerService: C8LoggerService) { }
+
+  log(message: string) {
+    console.log('Data Service LOG');
+    this.loggerService.log(message);
+  }
+
+  error(message: string) {
+    console.error('Data Service Error');
+    this.loggerService.error(message);
+  }
+}
