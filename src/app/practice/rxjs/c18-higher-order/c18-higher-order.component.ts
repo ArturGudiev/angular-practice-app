4e0c11a20888640793376c9cd5aee13003c85a2c
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-11 11:38:31 +0300

    Create c18

--- src/app/practice/rxjs/c18-higher-order/c18-higher-order.component.ts
+++ src/app/practice/rxjs/c18-higher-order/c18-higher-order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewRef} from '@angular/core';
+import {concatMap, fromEvent, interval, mergeMap, Observable, of, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-c18-higher-order',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./c18-higher-order.component.sass']
 })
 export class C18HigherOrderComponent implements OnInit {
+  @ViewChild('par') par!: ElementRef<any>;
+  createObservable =
+    (timeout: number, val: number, identifier: string) => new Observable((subscriber) => {
+      setTimeout(() => {
+        console.log(identifier, 'next')
+        subscriber.next(val);
+        console.log(identifier, 'complete')
+        subscriber.complete();
+      }, timeout);
+    })
 
-  constructor() { }
+  intervalObservable = interval(1000);
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    // this.t_first();
+    // this.t_second();
+    this.t_third();
+  }
+
+  private t_second() {
+    // of(1,2,3).pipe(
+    //   ****(i => this.createObservable(1000-100*i, i, `obs${i}`))
+    // ).subscribe(val => console.log('Outer observable', val))
+
+
+  }
+
+  private t_third() {
+    // of(1,2,3).pipe(
+    //   switchMap(i => this.createObservable(1000, i, `obs${i}`))
+    // ).subscribe(val => console.log('Outer observable', val))
+
+  }
 }
