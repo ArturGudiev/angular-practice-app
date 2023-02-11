c9b4b55aba0e21af25bd2322590ddd142c4af25d
Artur Gudiev <arturgudiev93@gmail.com>
2023-02-11 14:23:59 +0300

    Create c18

--- src/app/practice/services/c10-catch-error/c10.service.ts
+++ src/app/practice/services/c10-catch-error/c10.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class C10Service {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
+    // return this.http.get('http://localhost:3030').pipe(
+    //
+    // );
     return of([{x: 1, y: 2}, {x: 3, y: 4}])
   }
 }
